67b03d9cbd329a9c88aaf83eece747070f4127b2
Magic <Roger@rex42.com>
2025-08-20 04:40:51 -0500

    fdsa

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -1,13 +1,19 @@
 import Cocoa
 
-// MARK: - App Delegate
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var windowController: WindowController?
     
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        // This ensures your app runs as a regular GUI app
+        NSApp.setActivationPolicy(.regular)
+    }
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
-        windowController = WindowController(window: nil)  // Add 'window: nil'
+        windowController = WindowController()
         windowController?.showWindow(nil)
+        
+        // Force the window to appear
         windowController?.window?.makeKeyAndOrderFront(nil)
         NSApp.activate(ignoringOtherApps: true)
     }
--- App/WindowController.swift
+++ App/WindowController.swift
@@ -1,6 +1,5 @@
 import Cocoa
 
-// MARK: - Window Controller
 class WindowController: NSWindowController {
     
     override init(window: NSWindow?) {
@@ -14,14 +13,26 @@ class WindowController: NSWindowController {
         window.center()
         window.setFrameAutosaveName("MainWindow")
         
+        // Add these to ensure window is visible
+        window.isReleasedWhenClosed = false
+        window.level = .normal
+        
         super.init(window: window)
         
         let viewController = MainViewController()
         window.contentViewController = viewController
+        
+        // Make sure the window is visible
+        window.makeKeyAndOrderFront(nil)
     }
     
-    // Add this required initializer
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Override showWindow to ensure it's visible
+    override func showWindow(_ sender: Any?) {
+        super.showWindow(sender)
+        window?.makeKeyAndOrderFront(nil)
     }
 }
