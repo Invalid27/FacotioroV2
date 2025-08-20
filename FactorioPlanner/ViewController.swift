c266c5e8370055e2db9cd75c75aaba8416dbbd36
Magic <Roger@rex42.com>
2025-08-19 23:39:50 -0500

    Initial Commit

---
+++ FactorioPlanner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FactorioPlanner
+//
+//  Created by Daniel on 8/19/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ FactorioPlannerTests/FactorioPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FactorioPlannerTests.swift
+//  FactorioPlannerTests
+//
+//  Created by Daniel on 8/19/25.
+//
+
+import Testing
+@testable import FactorioPlanner
+
+struct FactorioPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
